6c845b0df3a0ad0f33a2cadd0cede424f11e53cc
lams 1474 <la.maiguas@uea.edu.ec>
2025-06-18 17:27:59 -0500

    Proyecto funcional, se agregaron 2 empleados mas y se probó ejecución

--- RegistroAportes/Program.cs
+++ RegistroAportes/Program.cs
@@ -4,19 +4,27 @@
     {
         Asociacion asociacion = new Asociacion(20);
 
-        Empleado emp1 = new Empleado(1, "Ana Martínez", "Secretaria", 10);
+        Empleado emp1 = new Empleado(1, "Piedad Cango", "Secretaria", 10);
         emp1.AgregarAporte(new Aporte(DateTime.Now, 50.0, "Cuota mensual"));
         emp1.AgregarAporte(new Aporte(DateTime.Now.AddDays(-10), 20.0, "Donación"));
 
-        Empleado emp2 = new Empleado(2, "Carlos Díaz", "Técnico", 10);
+        Empleado emp2 = new Empleado(2, "Kevin Maigua", "Técnico", 10);
         emp2.AgregarAporte(new Aporte(DateTime.Now, 60.0, "Cuota mensual"));
 
-        Empleado emp3 = new Empleado(3, "Elena Rivas", "Contadora", 10);
+        Empleado emp3 = new Empleado(3, "Bryan Maigua", "Contadora", 10);
         emp3.AgregarAporte(new Aporte(DateTime.Now.AddDays(-5), 75.5, "Aporte extraordinario"));
+
+        Empleado emp4 = new Empleado(4, "Camila Maigua", "Asistente", 10);
+        emp4.AgregarAporte(new Aporte(DateTime.Now.AddDays(-20), 30.0, "Aporte voluntario"));
+
+        Empleado emp5 = new Empleado(5, "Luis Maigua", "Presidente", 10);
+        emp5.AgregarAporte(new Aporte(DateTime.Now, 100.0, "Cuota anual"));
 
         asociacion.AgregarEmpleado(emp1);
         asociacion.AgregarEmpleado(emp2);
         asociacion.AgregarEmpleado(emp3);
+        asociacion.AgregarEmpleado(emp4);
+        asociacion.AgregarEmpleado(emp5);
 
         // Mostrar todos los aportes
         asociacion.MostrarReporteGeneral();
